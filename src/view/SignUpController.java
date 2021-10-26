2170112ec0cc6cdf3adfece3f00ac0c16812aba0
2dam <2dam@HZ375748.TartangaLH.eus>
2021-10-26 12:21:37 +0200

    Sign Up with email and full name validation

--- src/view/SignUpController.java
+++ src/view/SignUpController.java
@@ -6,12 +6,16 @@
 package view;
 
 import classes.User;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -24,7 +28,7 @@ import javafx.stage.WindowEvent;
  * @author 2dam
  */
 public class SignUpController {
-    
+
     @FXML
     private Button btnSignUp;
     //Button that is associated to the button 'btnSignUp' of the SignUpWindow.fxml
@@ -58,20 +62,24 @@ public class SignUpController {
     @FXML
     private Label lblPasswordMax;
     //Label that is associated to the label 'lblPasswordMax' of the SignUpWindow.fxml
-    
+
     private Stage stage;
     private User user;
-    
-    public void initStage(Parent root){
+
+    public void initStage(Parent root) {
         //stage.initModality(Modality.APPLICATION_MODAL); 
-        Scene scene=new Scene(root);
+        Scene scene = new Scene(root);
         stage.setResizable(false);
         stage.setScene(scene);
+        lblFullNameMax.setVisible(false);
+        lblEmailMax.setVisible(false);
+        lblLoginMax.setVisible(false);
+        lblPasswordMax.setVisible(false);
         hypLogIn.setOnAction(this::signIn);
         btnSignUp.setOnAction(this::signUp);
         stage.show();
     }
-    
+
     public User getUser() {
         return user;
     }
@@ -83,17 +91,39 @@ public class SignUpController {
     public void setStage(Stage stage) {
         this.stage = stage;
     }
-    
-    public void signIn(ActionEvent action){
+
+    public void signIn(ActionEvent action) {
         stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
     }
-    
-    public void signUp(ActionEvent action){
-        
-        stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/SignedInWindow.fxml"));
-        Stage stageSignIn=new Stage();
-        
+
+    public void signUp(ActionEvent action) {
+        try {
+            fullNameValidation();
+            stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/SignedInWindow.fxml"));
+            Stage stageSignIn = new Stage();
+        } catch (Exception ex) {
+            Logger.getLogger(SignUpController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
+    private void fullNameValidation() throws Exception {
+        if (txtFullName.getText().length() >= 255) {
+            throw new Exception("Maximum character limit reached!");
+        } else if (txtFullName.getText().trim().isEmpty()) {
+            throw new Exception("Empty Full Name field!");
+        }
+    }
+
+    private void emailValidation() throws Exception {
+        if (txtFullName.getText().trim().isEmpty()) {
+            throw new Exception("Empty Email field!");
+        } else if (txtEmail.getText().length() >= 255) {
+            throw new Exception("Maximum character limit reached!");
+        } else if (!txtEmail.getText().matches("[A-Za-z0-9._%+-]+@[A-Z0-9.-]+[a-z]+.com")) {
+            throw new Exception("Error, Email not valid!");
+        }
+
+    }
 }
